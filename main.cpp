ca56d0e8143361b64a82e0d92f0791075c4a7f9a
Saksham <saksham24b@gmail.com>
2025-05-27 18:08:59 +0530

    Added 2 player mode

--- main.cpp
+++ main.cpp
@@ -58,16 +58,17 @@ class Paddle{
     float x,y;
     float width, height;
     int speed;
+    int keyUp, keyDown;
 
     void Draw(){
         DrawRectangleRounded(Rectangle{x,y,width,height},0.8,0,WHITE);
     }
 
     void Update(){
-        if(IsKeyDown(KEY_UP)){
+        if(IsKeyDown(keyUp)){
             y = y - speed;
         }
-        if(IsKeyDown(KEY_DOWN)){
+        if(IsKeyDown(keyDown)){
             y = y + speed;
         }
         LimitMovement();
@@ -89,10 +90,10 @@ class CpuPaddle: public Paddle{
 };
 
 Ball ball;
-Paddle player;
+Paddle player, player2;
 CpuPaddle cpu;
 
-enum GameState{ MENU, PLAYING};
+enum GameState{ MENU, PLAYING_1, PLAYING_2};
 GameState gameState = MENU;
 
 int main () {
@@ -114,6 +115,16 @@ int main () {
     player.x = screen_width - player.width -10;
     player.y = screen_height/2 - player.height/2 -10;
     player.speed = 6;
+    player.keyUp = KEY_UP;
+    player.keyDown = KEY_DOWN;
+
+    player2.height = 120;
+    player2.width = 25;
+    player2.x = 10;
+    player2.y = screen_height/2 - cpu.height/2;
+    player2.speed = 6;
+    player2.keyUp = KEY_W;
+    player2.keyDown = KEY_S;
 
     cpu.height = 120;
     cpu.width = 25;
@@ -130,11 +141,17 @@ int main () {
             DrawText("[1] Player", screen_width/2 - 45, screen_height/2 + 100, 30, DARKGRAY);
             DrawText("[2] Player", screen_width/2 - 50, screen_height/2 + 140, 30, DARKGRAY);
             if(IsKeyPressed(KEY_SPACE)){
-                gameState = PLAYING;
+                gameState = PLAYING_1;
+            }
+            else if(IsKeyPressed(KEY_ONE)){
+                gameState = PLAYING_1;
+            }
+            else if(IsKeyPressed(KEY_TWO)){
+                gameState = PLAYING_2;
             }
             
         }
-        else if (gameState == PLAYING){
+        else if (gameState == PLAYING_1){
             //Updating
             ball.Update();
             player.Update();
@@ -158,7 +175,45 @@ int main () {
             DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
             DrawText(TextFormat("%i", cpu_score), screen_width / 4, 20, 80, WHITE);
             DrawText(TextFormat("%i", player_score), 3 * screen_width / 4, 20, 80, WHITE);
+            if(IsKeyPressed(KEY_M)){
+                gameState = MENU;
+                cpu_score = 0;
+                player_score = 0;
         }
+        }
+
+        else if (gameState == PLAYING_2){
+            //Updating
+            ball.Update();
+            player.Update();
+            player2.Update();
+
+            // Checking for collision
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+            {
+                ball.speed_x *= -1;
+            }
+
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height}))
+            {
+                ball.speed_x *= -1;
+            }
+
+            // Drawing
+            ball.Draw();
+            player2.Draw();
+            player.Draw();
+            DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+            DrawText(TextFormat("%i", cpu_score), screen_width / 4, 20, 80, WHITE);
+            DrawText(TextFormat("%i", player_score), 3 * screen_width / 4, 20, 80, WHITE);
+            if(IsKeyPressed(KEY_M)){
+                gameState = MENU;
+                cpu_score = 0;
+                player_score = 0;
+        }
+        }
+
+        
 
         EndDrawing();
     }
